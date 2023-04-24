635abbd84df7200ac0f867d2b2c8198cfb4a93e1
patrickdemurtas <patrickmurtass@gmail.com>
2023-04-23 21:26:45 -0300

    tryFix6: list-hotels

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -3,20 +3,16 @@ import ticketsRepository from '@/repositories/tickets-repository';
 import enrollmentRepository from '@/repositories/enrollment-repository';
 import hotelsRepository from '@/repositories/hotels-repository';
 
-async function checkTicketAndEnroll(userId: number) {
+async function listHotels(userId: number) {
   const infoEnroll = await enrollmentRepository.enrollById(userId);
   if (!infoEnroll) throw notFoundError();
 
   const infoTicket = await ticketsRepository.findTicketByEnrollmentId(infoEnroll.id);
   if (!infoTicket) throw notFoundError();
 
-  if (!infoTicket.TicketType.includesHotel || infoTicket.TicketType.isRemote || infoTicket.status !== 'PAID')
+  if (infoTicket.status !== 'PAID' || infoTicket.TicketType.isRemote || !infoTicket.TicketType.includesHotel) {
     throw paymentErr();
-  return;
-}
-
-async function listHotels(userId: number) {
-  await checkTicketAndEnroll(userId);
+  }
 
   const listOfHotels = await hotelsRepository.listHotels();
   if (listOfHotels.length === 0) throw notFoundError();
